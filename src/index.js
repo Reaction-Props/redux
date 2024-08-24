e2d35b52126f2bb9b7ea6a9544b691b4d9961dc3
React-Props <daniel.linker@intellicode-solutions.de>
2024-08-24 20:50:24 +0200

    combinereducr

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import {createStore} from "redux";
+import {createStore, combineReducers} from "redux";
 
 
-const initialState = {};
-const reducer = (state = initialState, action) => {
+
+const counterReducer = (state = 0, action) => {
     if (action.type === 'INCREMENT') {
         return state + 1
     }
     return state;
 }
 
+const todoReducer = (state = [], action) => {
+    if (action.type === 'ADD_TODO') {
+        return [...state, 'Lern redux']
+    }
+    return state
+
+}
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    todos: todoReducer,
+})
 
 const store = createStore(reducer)
 
+
+console.log(store.getState())
+
+
+store.dispatch({ type: 'INCREMENT'})
 
 console.log(store.getState())
